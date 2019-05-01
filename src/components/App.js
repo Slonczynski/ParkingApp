3dbd1cbaf4e229a670e0479eadabc086ead2cd78
Konrad <15710231+Slonczynski@users.noreply.github.com>
2019-05-01 22:31:43 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Tiles from "./Tiles";
+import "./scss/App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <head className="App-header" />
+      <head />
+      <button type="button" className="btn btn-primary">
+        Primary
+      </button>
+    </div>
+  );
+}
+
+export default App;
