368e2ebcfb8737f95c32361bbd239d7a4f9d2576
Konrad <15710231+Slonczynski@users.noreply.github.com>
2019-05-02 14:22:35 +0200

    Styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,49 @@
-import React from 'react';
-import Tile from './Tile';
-import './scss/App.scss';
+import React from "react";
+import Tile from "./Tile";
+import "./scss/App.scss";
 
 function App() {
   return (
     <div className="App">
-      <div className="d-flex">
-        <div className="d-flex justify-content-start">
-          <div className="previous-day">
-            <i className="big arrow right icon" />
+      <div className="d-flex justify-content-between align-items-center">
+        <div className="previous-day">
+          <div className="text-center">
+            <i className="circular inverted teal large arrow left icon" />
+            <div className="yesterday">
+              Wczoraj
+              <br />
+              30.04.2019
+            </div>
           </div>
         </div>
-        <div className="d-flex justify-content-end">
-          <div className="next-day">
-            <i className="big arrow left icon" />
+        <div className="text-center">
+          <div className="today">
+            Dzisiaj
+            <br /> 01.05.2019
+          </div>
+        </div>
+        <div className="next-day">
+          <div className="text-center">
+            <i className="circular inverted teal large arrow right icon" />
+            <div className="tomorrow">
+              Jutro
+              <br />
+              02.05.2019
+            </div>
           </div>
         </div>
       </div>
-      <div className="d-flex flex-row justify-content-center align-items-center">
-        <div className="p-2">Flex item 1</div>
-        <div className="p-2">Flex item 1</div>
+      <div className="parking-spots">
+        <div className="d-flex flex-column justify-content-center align-items-center">
+          <div className="p-2">Flex item 1</div>
+          <div className="p-2">Flex item 1</div>
+          <div className="p-2">Flex item 1</div>
+          <div className="p-2">Flex item 1</div>
+          <div className="p-2">Flex item 1</div>
+          <div className="p-2">Flex item 1</div>
+          <div className="p-2">Flex item 1</div>
+          <div className="p-2">Flex item 1</div>
+        </div>
       </div>
     </div>
   );
