bb9b4ba3e280073886baa3c9fe602cb6f260b822
Konrad <15710231+Olimpus444@users.noreply.github.com>
2019-05-02 11:47:22 +0200

    New flexbox

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,18 @@ import './scss/App.scss';
 function App() {
   return (
     <div className="App">
-      <div className="previous-day">
-        <i className="big arrow right icon" />
+      <div className="d-flex">
+        <div className="d-flex justify-content-start">
+          <div className="previous-day">
+            <i className="big arrow right icon" />
+          </div>
+        </div>
+        <div className="d-flex justify-content-end">
+          <div className="next-day">
+            <i className="big arrow left icon" />
+          </div>
+        </div>
       </div>
-      <div className="next-day">ASDASD </div>
       <div className="d-flex flex-row justify-content-center align-items-center">
         <div className="p-2">Flex item 1</div>
         <div className="p-2">Flex item 1</div>
