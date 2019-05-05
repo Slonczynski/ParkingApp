0feeabd521f016d008be1dc457f65f9b7ef1658e
Konrad <15710231+Slonczynski@users.noreply.github.com>
2019-05-05 20:08:49 +0200

    Add config

---
+++ src/components/config/fbConfig.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBblWFgGmUpS0dzCqkb18XgtonerAcwuUM",
+  authDomain: "parkingapp-5d88e.firebaseapp.com",
+  databaseURL: "https://parkingapp-5d88e.firebaseio.com",
+  projectId: "parkingapp-5d88e",
+  storageBucket: "parkingapp-5d88e.appspot.com",
+  messagingSenderId: "484914218682",
+  appId: "1:484914218682:web:3c5f27f76e386679"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings();
+
+export default firebase;
