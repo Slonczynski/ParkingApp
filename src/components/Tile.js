d331bbe0819d701b68d436330118beeb788e3144
Konrad <15710231+Olimpus444@users.noreply.github.com>
2019-05-10 08:05:05 +0200

    Add svg file, styling

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -6,16 +6,7 @@ const Tile = props => {
       <div className="parking-spot">
         <div className="text-center">
           {props.car}
-          <svg viewBox="0 0 100 200" xmlns="http://www.w3.org/2000/svg">
-            <rect
-              className="parking-place"
-              preserveAspectRatio="none"
-              x="0"
-              width="100"
-              height="160"
-              rx="15"
-            />
-          </svg>
+          <img src={require("./tile.svg")} alt="Kiwi standing on oval" />
         </div>
       </div>
     </div>
