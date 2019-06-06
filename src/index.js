066050d6f85b1a0f3b3e7da5dbad7373b7d8bdbb
Konrad <15710231+Slonczynski@users.noreply.github.com>
2019-06-06 20:35:23 +0200

    Add redux dev tools

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 import App from './components/App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducers from './components/store/reducers';
 
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers, composeWithDevTools())}>
     <App />
   </Provider>,
   document.getElementById('root')
