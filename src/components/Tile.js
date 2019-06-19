f0337811fbbebd31f8a564e815cf57f5f46a011c
unknown <15710231+Slonczynski@users.noreply.github.com>
2019-06-19 17:04:20 +0200

    Refactor Tile into class component

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -2,25 +2,43 @@ import React from 'react';
 import { Label, Icon } from 'semantic-ui-react';
 import './scss/Tile.scss';
 
-const Tile = props => {
-  return (
-    <div className="parking-spot">
-      <div className="text-center">
-        <div className="spot-number">{props.car}</div>
+class Tile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      1: '',
+      2: '',
+      3: '',
+      4: '',
+      5: '',
+      6: '',
+      7: '',
+      8: ''
+    };
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div className="parking-spot">
+        <div className="text-center">
+          <div className="spot-number">{this.props.car}</div>
+        </div>
+        <div className="text-center">
+          <img
+            className="parking-place"
+            src={require('./tile.svg')}
+            alt="parking-pace"
+          />
+          <Label className="spot-occupant" size="large">
+            Konrad
+            <Icon name="delete" />
+          </Label>
+        </div>
       </div>
-      <div className="text-center">
-        <img
-          className="parking-place"
-          src={require('./tile.svg')}
-          alt="parking-pace"
-        />
-        <Label className="spot-occupant" size="large">
-          Konrad
-          <Icon name="delete" />
-        </Label>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Tile;
