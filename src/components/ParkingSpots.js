66333d58c4ec7c2279d844040f0ebde5d08788d2
Konrad <15710231+Olimpus444@users.noreply.github.com>
2019-06-21 12:25:50 +0200

    Vol. 1

--- src/components/ParkingSpots.js
+++ src/components/ParkingSpots.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { DateTime } from 'luxon';
 import 'semantic-ui-css/semantic.min.css';
 import { Grid } from 'semantic-ui-react';
 
@@ -8,6 +9,34 @@ import ActionModal from './ActionModal';
 import './scss/ParkingSpots.scss';
 
 class ParkingSpots extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      freeSpotIds: [],
+      occupiedSpotIds: []
+    };
+  }
+  componentDidUpdate() {
+    const currentData = DateTime.fromISO(
+      this.props.switcherReducer.currentDay.timestamp
+    ).toFormat('dd-MM-yyyy');
+
+    if (
+      this.props.firestoreReducer.ordered['spots-collection']['0'][
+        currentData
+      ] == null &&
+      this.props.firestoreReducer.ordered['spots-collection']['0'][
+        currentData
+      ] === undefined
+    ) {
+      if (this.state.freeSpotIds !== ['1', '2', '3', '4', '5', '6', '7', '8'])
+        return this.setState({
+          ...this.state,
+          freeSpotIds: ['1', '2', '3', '4', '5', '6', '7', '8']
+        });
+    }
+  }
+
   render() {
     return (
       <div className="spots">
@@ -15,7 +44,14 @@ class ParkingSpots extends React.Component {
           <Separator spotsType="Tymczasowe:" />
           <Grid stackable centered relaxed columns={3}>
             <Grid.Column>
-              <ActionModal car="1." />
+              <ActionModal
+                car="1."
+                className={
+                  this.state.freeSpotIds.includes('1')
+                    ? 'parking-place-free'
+                    : 'parking-place-occupied'
+                }
+              />
             </Grid.Column>
             <Grid.Column>
               <ActionModal car="2." />
