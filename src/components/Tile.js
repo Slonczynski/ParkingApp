51db00b96d79a2f54856ca64a1ea424d29b4ccd6
Konrad <15710231+Slonczynski@users.noreply.github.com>
2019-06-25 22:57:33 +0200

    Fix label

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -3,26 +3,7 @@ import { Label, Icon } from 'semantic-ui-react';
 import './scss/Tile.scss';
 
 class Tile extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: {}
-    };
-  }
-
-  componentDidUpdate() {
-    if (this.props.name !== '') {
-      if (this.props.name !== this.state.name) {
-        this.setState({
-          name: this.props.name
-        });
-      }
-    }
-  }
-
   render() {
-    console.log(this.state.name);
-    console.log(this.props.className);
     return (
       <div className="parking-spot">
         <div className="text-center">
@@ -34,8 +15,7 @@ class Tile extends React.Component {
             src={require('./tile.svg')}
             alt="parking-place"
           />
-          {Object.entries(this.state.name).length === 0 &&
-          this.state.name.constructor === Object ? null : (
+          {this.props.className === 'parking-place-free' ? null : (
             <Label className="spot-occupant" size="large">
               {this.props.name}
               <Icon name="delete" />
