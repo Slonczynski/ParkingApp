fa59928772de8819a403ba5ba14ad8d77cc8d186
Konrad <15710231+Slonczynski@users.noreply.github.com>
2019-07-03 14:17:37 +0200

    Remove bootstrap classes, code cleanup

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Label, Icon } from 'semantic-ui-react';
+import { Grid, Label, Icon } from 'semantic-ui-react';
 import './scss/Tile.scss';
 
 class Tile extends React.Component {
@@ -21,22 +21,19 @@ class Tile extends React.Component {
   render() {
     return (
       <div className="parking-spot">
-        <div className="text-center">
-          <div className="spot-number">{this.props.car}</div>
-        </div>
-        <div className="text-center">
-          <img
-            className={this.state.parkingClassname}
-            src={require('./tile.svg')}
-            alt="parking-place"
-          />
-          {this.props.className === 'parking-place-free' ? null : (
-            <Label className="spot-occupant" size="big">
-              {this.props.name}
-              <Icon name="delete" />
-            </Label>
-          )}
-        </div>
+        <div className="spot-number">{this.props.car}</div>
+
+        <img
+          className={this.state.parkingClassname}
+          src={require('./tile.svg')}
+          alt="parking-place"
+        />
+        {this.props.className === 'parking-place-free' ? null : (
+          <Label className="spot-occupant" size="big">
+            {this.props.name}
+            <Icon name="delete" />
+          </Label>
+        )}
       </div>
     );
   }
