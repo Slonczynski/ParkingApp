871b0c38d3d045424fc69908ccb97ae50e1d0af0
Konrad <15710231+Slonczynski@users.noreply.github.com>
2019-07-06 16:09:22 +0200

    Add Weekday

--- src/components/Weekday.js
+++ src/components/Weekday.js
@@ -1,25 +1,39 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { DateTime } from 'luxon';
-import { Segment } from 'semantic-ui-react';
+import { Grid, Segment } from 'semantic-ui-react';
+
+import './scss/Weekday.scss';
 
 class Weekday extends React.Component {
   render() {
     const currentDate = DateTime.fromISO(
       this.props.switcherReducer.currentDay.timestamp
-    );
+    ).weekday;
+    const days = {
+      1: 'Poniedziałek',
+      2: 'Wtorek',
+      3: 'Środa',
+      4: 'Czwartek',
+      5: 'Piątek',
+      6: 'Sobota',
+      7: 'Niedziela'
+    };
 
     return (
-      <Segment
-        compact
-        padded
-        size="large"
-        textAlign="center"
-        inverted
-        color="green"
-      >
-        <span>{dayOfTheWeek}</span>
-      </Segment>
+      <div className="weekday-container">
+        <Grid centered>
+          <Segment
+            compact
+            size="large"
+            textAlign="center"
+            inverted
+            color="blue"
+          >
+            <span className="weekday">{days[currentDate]}</span>
+          </Segment>
+        </Grid>
+      </div>
     );
   }
 }
