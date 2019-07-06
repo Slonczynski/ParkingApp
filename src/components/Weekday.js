5e3bf96f8509b377276cbc7af52bf1ac40b22312
Konrad <15710231+Slonczynski@users.noreply.github.com>
2019-07-06 14:18:39 +0200

    Add segment

--- src/components/Weekday.js
+++ src/components/Weekday.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { DateTime } from 'luxon';
+import { Segment } from 'semantic-ui-react';
 
 class Weekday extends React.Component {
   render() {
@@ -8,7 +9,18 @@ class Weekday extends React.Component {
       this.props.switcherReducer.currentDay.timestamp
     );
 
-    return <span>{dayOfTheWeek}</span>;
+    return (
+      <Segment
+        compact
+        padded
+        size="large"
+        textAlign="center"
+        inverted
+        color="green"
+      >
+        <span>{dayOfTheWeek}</span>
+      </Segment>
+    );
   }
 }
 
