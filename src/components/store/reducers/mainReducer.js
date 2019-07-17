c9a3937138a258c2572cf1d14fd40291e0bf5f8b
Konrad <15710231+Slonczynski@users.noreply.github.com>
2019-07-17 19:06:11 +0200

    Code cleanup + package update

--- src/components/store/reducers/mainReducer.js
+++ src/components/store/reducers/mainReducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import switcherReducer from './switcherReducer';
-import databaseReducer from './databaseReducer';
 import { firestoreReducer } from 'redux-firestore';
 
 export default combineReducers({
   firestoreReducer,
-  switcherReducer,
-  databaseReducer
+  switcherReducer
 });
